d01b47114987f397ae3f3c0e86a681937165d8c2
Andriana Aivazians <Aaivazi000@citymail.cuny.edu>
2018-10-22 22:56:57 -0400

    Image can be uploaded but not yet to parse

--- Instaglam/View Controllers/CaptureViewController.swift
+++ Instaglam/View Controllers/CaptureViewController.swift
@@ -8,8 +8,64 @@
 
 import UIKit
 
-class CaptureViewController: UIViewController {
+class CaptureViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    //Declarations and UI Outlets
+    @IBOutlet weak var postImageView: UIImageView!
+    var selectedforpostUIImage: UIImage?
+    
+    
+    //Action when postImageView is tapped
+    @IBAction func onimageTap(_ sender: Any) {
+        selectPhoto()
+    }
+    
+    //Select Photo function
+    func selectPhoto() {
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        vc.sourceType = UIImagePickerController.SourceType.photoLibrary
+        
+        self.present(vc, animated: true, completion: nil)
+        
+        //Un comment below if doing demontration not on Simulator.
+        /*if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            print("Camera is available 📸")
+            vc.sourceType = .camera
+        } else {
+            print("Camera 🚫 available so we will use photo library instead")
+            vc.sourceType = .photoLibrary
+        }
+         */
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // Get the image captured by the UIImagePickerController
+        let originalImage = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        let editedImage = info[UIImagePickerController.InfoKey.editedImage] as! UIImage
+        
+        // Set image in the UI Image view in CaptureViewController
+        postImageView.image = editedImage
+        
+        // Dismiss UIImagePickerController to go back to your original view controller
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //Function when user clicks Submit Button
+    @IBAction func onSubmitTap(_ sender: Any) {
+        
+        //Take image in UI Image View and set to var
+        selectedforpostUIImage = postImageView.image
+        
+        //Check if photo size is under 10MB and reduce if necessary
+        
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
